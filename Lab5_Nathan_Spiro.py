6ede1a9ecba6131e6df6f8678e60b76a9c09f931
Nathan Sheldrake <nathansheldrake151@gmail.com>
2025-03-27 18:43:11 -0400

    Created file

--- Lab5_Nathan_Spiro.py
+++ Lab5_Nathan_Spiro.py
@@ -30,7 +30,7 @@ class SeleniumClass(unittest.TestCase):
         sleep(3)
 
         # switch to frame 0
-        driver.switch_to.frame(0)
+        #driver.switch_to.frame(0)
 
         # Create an ActionChains Object
         action = ActionChains(driver)
@@ -40,20 +40,20 @@ class SeleniumClass(unittest.TestCase):
         women_tab = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/nav/ul/li[2]/a/span[2]')
         # Hover cursor over the women tab using action chains
         action.move_to_element(women_tab).perform()
-        sleep(1)
+        sleep(2)
 
         # Hover cursor on Tops
         tops_option = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div[2]/nav/ul/li[2]/ul/li[1]/a/span[2]')
         # Hover cursor over the tops option using action chains
         action.move_to_element(tops_option).perform()
-        sleep(1)
+        sleep(2)
 
         # Click on Hoodies & Sweatshirts
         # Find the Hoodies & Sweatshirts option via XPATH using the find element method
         hoodies_option = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div[2]/nav/ul/li[2]/ul/li[1]/ul/li[2]/a/span')
         # Click on Hoodies & Sweatshirts
         hoodies_option.click()
-        sleep(1)
+        sleep(2)
 
         # Select the appropriate Style, Size, Price Range, Color and Material
         # Style (Pullover)
@@ -62,41 +62,45 @@ class SeleniumClass(unittest.TestCase):
 
         style_pullover = driver.find_element(By.XPATH, '//*[@id="narrow-by-list"]/div[1]/div[2]/ol/li[3]/a')
         style_pullover.click()
-        sleep(1)
+        sleep(2)
 
         # Size (M)
         size = driver.find_element(By.XPATH, '//*[@id="narrow-by-list"]/div[2]/div[1]')
         size.click()
-        sleep(1)
+        sleep(2)
 
         size_m = driver.find_element(By.XPATH, "//a[@aria-label='M']//div[contains(@class,'swatch-option text')][normalize-space()='M']")
-        action.click(size_m).perform()
-        sleep(1)
+        #action.click(size_m).perform()
+        size_m.click()
+        sleep(2)
 
         # Price Range (50 - 59)
         price = driver.find_element(By.XPATH, '//*[@id="narrow-by-list"]/div[11]/div[1]')
         price.click()
 
         price_range = driver.find_element(By.XPATH, "//span[normalize-space()='$59.99']")
-        action.click(price_range).perform()
-        sleep(1)
+        #action.click(price_range).perform()
+        price_range.click()
+        sleep(2)
 
         # Color (Purple)
         color = driver.find_element(By.XPATH, '//*[@id="narrow-by-list"]/div[4]/div[1]')
         color.click()
 
         color_purple = driver.find_element(By.XPATH, "//a[@aria-label='Purple']//div[contains(@class,'swatch-option color')]")
-        action.click(color_purple).perform()
-        sleep(1)
+        #action.click(color_purple).perform()
+        color_purple.click()
+        sleep(2)
 
         # Material (Polyester)
         material = driver.find_element(By.XPATH, '//*[@id="narrow-by-list"]/div[7]/div[1]')
         material.click()
-        sleep(1)
+        sleep(2)
 
         material_polyester = driver.find_element(By.XPATH, "//a[contains(text(),'Polyester')]")
-        action.click(material_polyester).perform()
-        sleep(1)
+        #action.click(material_polyester).perform()
+        material_polyester.click()
+        sleep(2)
 
     def test_to_add_item_to_cart(self):
 
@@ -106,17 +110,20 @@ class SeleniumClass(unittest.TestCase):
         driver = self.driver
 
         # Open the following URL in the browser
-        #driver.get("https://magento.softwaretestingboard.com/")
-        #sleep(3)
+        driver.get("https://magento.softwaretestingboard.com/")
+        sleep(3)
 
         # [ You may have to switch to frame]
         # switch to frame 0
-        #driver.switch_to.frame(0)
+        #iframe = driver.find_element(By.XPATH, "/html/body/iframe")
+        #iframe = driver.find_element(By.TAG_NAME, "iframe")
+        driver.switch_to.frame(1)
+        sleep(2)
 
         # ------------------
 
         # Selected item (Autumn Pullie)
-        #autumn_pullie = driver.find_element(By.CLASS_NAME, "product-image-photo")
+        #autumn_pullie = driver.find_element(By.XPATH, '//*[@id="maincontent"]/div[3]/div[1]/div[3]/ol/li[10]/div/a')
         autumn_pullie = driver.find_element(By.XPATH, "//img[@alt='Autumn Pullie-M-Purple']")
         autumn_pullie.click()
 
@@ -147,6 +154,6 @@ class SeleniumClass(unittest.TestCase):
 
     # Finally close the browser
     @classmethod
-    def tearDown(cls):
+    def tearDownClass(cls):
         cls.driver.quit()
 
